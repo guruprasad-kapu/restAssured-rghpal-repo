2b7eebd57c0d0c485286aec292260693b5a8ce7c
gurup <guruprasad.kapu.it@gmail.com>
2024-03-16 12:31:02 -0400

    Testing RestFul APIS using Map objects to send the post request.

---
+++ testWebServices/src/test/java/com/auto/testWebServices/testAPIUsingMaps.java
@@ -0,0 +1,13 @@
+package com.auto.testWebServices;
+
+import org.testng.annotations.Test;
+
+public class testAPIUsingMaps 
+{
+	@Test
+	public void testGetRequestMaps()
+	{
+		System.out.println("New file. Yet to stage and commit.");
+	}
+
+}
